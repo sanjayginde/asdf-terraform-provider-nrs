4641d18a48ee080bbf6d784c9fa425e053186185
Sanjay Ginde <sanjayginde@gmail.com>
2018-10-12 11:02:00 -0400

    First version of ASDF plugin for Terraform New Relic Synthetics provider

---
+++ test/dev_reset.sh
@@ -0,0 +1,2 @@
+asdf plugin-remove terraform-provider-nrs
+asdf plugin-add terraform-provider-nrs ./
---
+++ test/test_install.sh
@@ -0,0 +1,2 @@
+mkdir -p tmp/0.1.0
+ASDF_INSTALL_TYPE=terraform-provider-nrs ASDF_INSTALL_VERSION=0.1.0 ASDF_INSTALL_PATH=./tmp/0.1.0 ./bin/install
